c7cc7dfe4b5861f47ab0fd97815585f2fa61ab67
sherryysj <45263452+sherryysj@users.noreply.github.com>
2024-01-05 23:45:39 +1100

    add boost speed

--- Snow Boarder - 2D/Assets/Scripts/PlayerController.cs
+++ Snow Boarder - 2D/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,18 +11,35 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     private float torqueAmount = 5f;
     private int reloadDelayTime = 1;
+    private float boostSpeed = 40f;
+    private float defaultSpeed = 20f;
+    private SurfaceEffector2D surfaceEffector2D;
 
     // Start is called before the first frame update
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update() {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RotatePlayer() {
         if (Input.GetKey(KeyCode.LeftArrow)) {
             rb2d.AddTorque(torqueAmount);
         } else if (Input.GetKey(KeyCode.RightArrow)) {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    private void RespondToBoost() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            surfaceEffector2D.speed = boostSpeed;
+        } else {
+            surfaceEffector2D.speed = defaultSpeed;
         }
     }
 
